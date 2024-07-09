cae77bb83591ffddab9cef84a750f245a9e1a6c7
gadgetmies <gadgetmies@gmail.com>
2024-07-09 21:32:03 +0300

    WIP

--- db-init.sql
+++ db-init.sql
@@ -1,11 +1,16 @@
 create
 extension vector;
+
+create
+extension if not exists unaccent;
+
 create table track
 (
     track_id           serial primary key,
     track_path         text UNIQUE,
     track_artist       text,
     track_title        text,
+    track_comment      text,
     track_isrc         text,
     track_key          text,
     track_bpm          numeric,
@@ -21,11 +26,12 @@ create table track_embedding
 (
     track_id               integer references track (track_id) ON DELETE CASCADE UNIQUE NOT NULL,
     track_embedding_vector vector(1280) not null,
-    track_embedding_type   embedding_type not null,
+    track_embedding_type   embedding_type                                               not null,
     unique (track_id, track_embedding_type)
 );
 
-create table track_spotify_audio_features (
-    track_id integer references track(track_id) ON DELETE CASCADE UNIQUE NOT NULL,
-    track_spotify_audio_features JSON NOT NULL
+create table track_spotify_audio_features
+(
+    track_id                     integer references track (track_id) ON DELETE CASCADE UNIQUE NOT NULL,
+    track_spotify_audio_features JSON                                                         NOT NULL
 );
--- prompt.py
+++ prompt.py
@@ -9,23 +9,12 @@ from configparser import ConfigParser
 import psycopg2 as psycopg2
 import traceback
 from tabulate import tabulate
+from datetime import date
 
-
-def print_commands():
-    print('''
-Usage
------
-<enter>: Record and find suitable tracks using the set key and BPM as well as the features extracted from input
-b 140:   Set BPM to 140
-k 1A:    Set current key to 1A (you can use OpenKey (1m), Camelot (1A) and standard keys (Abm))
-f:       Set key format (standard, camelot, openkey)
-a:       Select audio device
-q:       Quit
-    ''')
-
-
-bpm = 140
-key = 0
+current_year = int(date.today().strftime("%Y"))
+earliest_year = 1900
+bpm = None
+key = None
 key_format = 'camelot'
 keys = {
     'camelot': ['1A', '2A', '3A', '4A', '5A', '6A', '7A', '8A', '9A', '10A', '11A', '12A', '1B', '2B', '3B', '4B', '5B',
@@ -35,7 +24,31 @@ keys = {
     'standard': ['Abm', 'Ebm', 'Bbm', 'Fm', 'Cm', 'Gm', 'Dm', 'Am', 'Em', 'Bm', 'F#m', 'Dbm', 'B', 'F#', 'Db', 'Ab',
                  'Eb', 'Bb', 'F', 'C', 'G', 'D', 'A', 'E']
 }
+
+
+def print_commands():
+    print('''
+Usage
+-----
+a:       Select audio device
+f:       Set key format (standard, camelot, openkey)
+
+b 140:   Set BPM to 140
+k 1A:    Set current key to 1A (you can use OpenKey (1m), Camelot (1A) and standard keys (Abm))
+y:       Set earliest release year filter
+
+<enter>: Record and find suitable tracks using the set key and BPM as well as the features extracted from input
+s query: Search for track matching 'query'
+s 1:     Search for compatible tracks for track with index 1 in the most recent track search results
+s:       Search for compatible tracks for currently selected track with updated filters
+l:       List the most recent track search results               
+q:       Quit
+    ''')
+
+
 key_formats = list(keys.keys())
+results = []
+selected_track = {}
 
 
 def load_config(filename='database.ini', section='postgresql'):
@@ -79,6 +92,7 @@ def select_key_format():
             print(x)
             print("ERROR: Invalid selection. Please provide a single number.")
 
+
 def select_audio_device():
     while True:
         devices = sd.query_devices()
@@ -92,7 +106,91 @@ def select_audio_device():
             print('ERROR: Unable to parse input. Please provide a single number.')
 
 
-def record_audio():
+result_headers = ['#', 'ID', 'Artist', 'Title', 'Comment', 'BPM', 'Key']
+
+
+def search(query):
+    with conn.cursor() as cur:
+        try:
+            cur.execute(f"""
+SELECT track_id AS id, track_artist AS artist, track_title AS title, track_comment as comment, track_bpm AS bpm, track_key AS key
+FROM track
+WHERE
+  TO_TSVECTOR(
+          'simple',
+          unaccent(track_title || ' ' ||
+                   COALESCE(track_title, '') || ' ' ||
+                   COALESCE(track_artist, ' ') || ' ' ||
+                   COALESCE(track_album, ' ') || ' ' ||
+                   COALESCE(track_label, ''))) @@
+  websearch_to_tsquery('simple', unaccent('{query}'))
+LIMIT 20
+            """)
+            records = cur.fetchall()
+            cur.close()
+            return records
+
+        except(psycopg2.DatabaseError, Exception) as error:
+            print("Search failed:")
+            print(error)
+            cur.close()
+            return []
+
+
+def select_track(results):
+    while True:
+        selected = input('Track number > ')
+        try:
+            track_number = int(selected)
+            if track_number < 0 or track_number >= len(results):
+                print(f"ERROR: Invalid track number. Please provide number between 1-{len(results) - 1}")
+                continue
+            return results[track_number]
+        except Exception as x:
+            print(x)
+            print(f'ERROR: Unable to parse input. Please provide a single track number between 1-{len(results) - 1}')
+
+
+def find_suitable_tracks(selected_track):
+    with conn.cursor() as cur:
+        suitable_keys = keys['camelot']
+        if key is not None:
+            num = int(keys['camelot'][key][0:-1])
+            next_num = (num + 1) % 12
+            prev_num = (num - 1)
+            if prev_num < 1:
+                prev_num += 12
+            suitable_keys = [f'{num}A', f'{num}B', f'{next_num}A', f'{next_num}B', f'{prev_num}A', f'{prev_num}B']
+        try:
+            cur.execute(f"""
+WITH selected_track AS (
+    SELECT track_embedding_vector, track_artist 
+    FROM track_embedding NATURAL JOIN track 
+    WHERE track_id = %s AND track_embedding_type = 'multi'
+)
+SELECT track_id AS id, t.track_artist AS artist, track_title AS title, track_comment AS comment, track_bpm AS bpm, track_key AS key
+FROM track t NATURAL JOIN track_embedding te, selected_track
+WHERE
+    track_id <> %s
+    AND track_key = ANY(%s)
+    AND (%s IS NULL OR (ABS(1 - track_bpm / %s) < 0.1 OR ABS(1 - track_bpm * 2 / %s) < 0.1 OR ABS(1 - track_bpm / (%s * 2)) < 0.1))    AND t.track_artist <> selected_track.track_artist
+    AND track_release_date > to_date(%s::text, 'YYYY')
+ORDER BY
+    te.track_embedding_vector <-> selected_track.track_embedding_vector 
+LIMIT 20
+                    """, [selected_track[0], selected_track[0], suitable_keys, bpm, bpm, bpm, bpm, earliest_year])
+            records = cur.fetchall()
+            cur.close()
+            return records
+
+        except (psycopg2.DatabaseError, Exception) as error:
+            print(error)
+            traceback.print_exc()
+            cur.close()
+            conn.rollback()
+
+
+def similarity_search():
     sampling_frequency = 44100
     duration = 5
     print("Recording")
@@ -107,34 +205,37 @@ def record_audio():
                                            output="PartitionedCall:1")
     print("Processing audio")
     embeddings = model(audio)
-    print(embeddings)
     print("Analysis done, searching...")
     with conn.cursor() as cur:
-        num = int(keys['camelot'][key][0:-1])
-        next_num = (num + 1) % 12
-        prev_num = (num - 1)
-        if prev_num < 1:
-            prev_num += 12
-        suitable_keys = [f'{num}A', f'{num}B', f'{next_num}A', f'{next_num}B', f'{prev_num}A', f'{prev_num}B']
+        suitable_keys = keys['camelot']
+        if key is not None:
+            num = int(keys['camelot'][key][0:-1])
+            next_num = (num + 1) % 12
+            prev_num = (num - 1)
+            if prev_num < 1:
+                prev_num += 12
+            suitable_keys = [f'{num}A', f'{num}B', f'{next_num}A', f'{next_num}B', f'{prev_num}A', f'{prev_num}B']
         try:
             cur.execute(f"""
-SELECT track_artist AS artist, track_title AS title, track_bpm AS bpm, track_key AS key
+SELECT track_id AS id, track_artist AS artist, track_title AS title, track_comment AS comment, track_bpm AS bpm, track_key AS key
 FROM track NATURAL JOIN track_embedding
 WHERE 
     track_key = ANY(%s)
-    AND ABS(1 - track_bpm / %s) < 0.1
+    AND (%s IS NULL OR (ABS(1 - track_bpm / %s) < 0.1 OR ABS(1 - track_bpm * 2 / %s) < 0.1 OR ABS(1 - track_bpm / (%s * 2)) < 0.1))
+    AND track_release_date > to_date(%s::text, 'YYYY')
 ORDER BY
     track_embedding_vector <-> '{embeddings.T.mean(1).tolist()}'
 LIMIT 20
-            """, [suitable_keys, bpm])
+            """, [suitable_keys, bpm, bpm, bpm, bpm, earliest_year])
             records = cur.fetchall()
-            print(tabulate(records, headers="keys", tablefmt="psql"))
+            cur.close()
+            print(tabulate(records, headers=result_headers, tablefmt="psql", showindex="always"))
+            return records
 
         except (psycopg2.DatabaseError, Exception) as error:
             print(error)
             traceback.print_exc()
             cur.close()
-            conn.rollback()
 
 
 if __name__ == '__main__':
@@ -143,9 +244,10 @@ if __name__ == '__main__':
     print_commands()
     while True:
         devices = sd.query_devices()
-        user_input = input(f'{devices[sd.default.device[1]]["name"]} {bpm} {keys[key_format][key]} > ')
+        user_input = input(
+            f'{devices[sd.default.device[1]]["name"]} {bpm if bpm else "any_BPM"} {keys[key_format][key] if key else "any_key"} >{earliest_year} >>> ')
         if user_input == '':
-            record_audio()
+            results = similarity_search()
             continue
 
         cmd, *args = shlex.split(user_input)
@@ -156,6 +258,40 @@ if __name__ == '__main__':
         elif cmd == 'a':
             select_audio_device()
 
+        elif cmd == 's':
+            search_results = []
+            if len(args) == 0 or (len(args) == 1 and args[0].isnumeric()):
+                if len(args) == 1:
+                    index = int(args[0])
+                    result_count = len(results)
+                    if result_count == 0:
+                        print(
+                            'ERROR: No tracks to choose from. Please run text search (s command) or similarity search \
+(empty command) first')
+                        continue
+                    elif index > result_count:
+                        print(f'ERROR: track index out of bounds. Please provide value between 0 and {result_count}')
+                        continue
+                    selected_track = results[index]
+                    if selected_track[4]:
+                        bpm = selected_track[4]
+                    if selected_track[5]:
+                        key = keys['camelot'].index(selected_track[5])
+
+                maybe_comment = selected_track[3]
+                print(
+                    f"Selected track: {selected_track[1]} - {selected_track[2]} \
+{f'({maybe_comment})' if maybe_comment else ''} [{selected_track[4]} {selected_track[5]}]")
+                search_results = find_suitable_tracks(selected_track)
+            else:
+                search_results = search(' '.join(args))
+
+            if len(search_results) > 1:
+                print(tabulate(search_results, headers=result_headers, tablefmt="psql", showindex="always"))
+                results = search_results
+            else:
+                print("No suitable tracks found")
+
         elif cmd == 'f':
             key_format = select_key_format()
 
@@ -165,6 +301,19 @@ if __name__ == '__main__':
             except Exception as x:
                 print(x)
                 print(f"ERROR: Invalid selection. Please provide {key_format} key")
+
+        elif cmd == 'l':
+            print(tabulate(results, headers=result_headers, tablefmt="psql", showindex="always"))
+
+        elif cmd == 'y':
+            try:
+                year = int(args[0])
+                if (year < 1900 or year > current_year):
+                    print("ERROR: Invalid year provided. Please provide value between the current year and 1900")
+                earliest_year = year
+            except Exception as x:
+                print(x)
+                print("ERROR: Invalid year provided. Please provide value between the current year and 1900")
 
         elif cmd == 'q':
             print('Bye!')
